5414446e9685430a0e564dd0958b44cc94a70283
Samandar-dev-hack <soatmurotovsamandar2327@gmail.com>
2025-05-17 17:45:27 -0700

    dmofmodmfodf

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,24 @@
+import java.io.*;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        File file  = new File("C:\\Users\\samnadar\\OneDrive\\Desktop\\password.csv");
+        FileInputStream fileInputStream = new FileInputStream(file);
+         int size  = fileInputStream.available();
+         byte [] bytes = new byte[size];
+         fileInputStream.read(bytes);
+         fileInputStream.close();
+        for (byte aByte : bytes) {
+            System.out.print((char)aByte);
 
-// hazirli kun
+        }
+         File file1 = new File("C:\\Users\\samnadar\\OneDrive\\Desktop\\hujjatlar.csv");
+         file1.createNewFile();
+        FileOutputStream fileOutputStream = new FileOutputStream(file1);
+        fileOutputStream.write(bytes);
+        fileOutputStream.close();
+
     }
 }
